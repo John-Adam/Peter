9245826fd6f3bf5a70e50e56b44c5fda4f6baf1c
John-Adam <39347010+John-Adam@users.noreply.github.com>
2018-05-17 10:02:33 +0200

    First Commint
    Added Unity-Projekt to GitHub

---
+++ New Unity Project/Assets/EnemyHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+public class EnemyHealth : MonoBehaviour 
+{ 
+	public int health = 100; void ApplyDamage (int damage) 
+	{ 
+		health -= damage; 
+		if (health <= 0) 
+		{
+			Dead(); 
+		} 
+	} 
+	void Dead() 
+	{ Destroy(gameObject); 
+	} 
+}
---
+++ New Unity Project/Assets/MeleeSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+public class MeleeSystem : MonoBehaviour 
+{ 
+	public int damage = 25; public float distance; public float maxDistance = 1.5f; private RaycastHit hit; void Update () 
+	{ 
+		if (Input.GetButtonDown("Fire1")) 
+		{ 
+			if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+			{ 
+				distance = hit.distance; if(distance < maxDistance) 
+				{ 
+					hit.transform.SendMessage("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+				} 
+			}
+		}
+	}
+}
